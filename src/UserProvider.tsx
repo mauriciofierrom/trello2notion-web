6ef348c7b899cb95c148c37da08359d946c990ac
Mauricio Fierro <mauriciofierrom@gmail.com>
2024-04-22 21:08:45 -0500

    Add UserProvider
    Responds to change in authentication state to set the user context
    
    - Whether the user is logged in
    - User information (the email for now)

---
+++ src/UserProvider.tsx
@@ -0,0 +1,57 @@
+import React, { createContext, useEffect, useState, useMemo } from "react";
+import authgear from "@authgear/web";
+
+interface UserContextValue {
+  isLoggedIn: boolean;
+  email: string;
+}
+
+export const UserContext = createContext<UserContextValue>({
+  isLoggedIn: false,
+  email: ""
+});
+
+interface UserContextProviderProps {
+  children: React.ReactNode;
+}
+
+const UserContextProvider: React.FC<UserContextProviderProps> = ({
+  children,
+}) => {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>("")
+
+  useEffect(() => {
+    authgear.delegate = {
+      onSessionStateChange: (container) => {
+        const sessionState = container.sessionState;
+        if (sessionState === "AUTHENTICATED") {
+          setIsLoggedIn(true);
+          try {
+            authgear.fetchUserInfo().then((userInfo) => {
+              console.log(userInfo);
+              setEmail(userInfo.email || "");
+            })
+          } catch(e) {
+            console.log(e)
+          }
+        } else {
+          setIsLoggedIn(false);
+        }
+      },
+    };
+  }, [setIsLoggedIn, setEmail]);
+
+  const contextValue = useMemo<UserContextValue>(() => {
+    return {
+      isLoggedIn,
+      email
+    };
+  }, [isLoggedIn, email]);
+
+  return (
+    <UserContext.Provider value={contextValue}>{children}</UserContext.Provider>
+  );
+};
+
+export default UserContextProvider;
