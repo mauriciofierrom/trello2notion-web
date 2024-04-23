e50423f2f33a66690dad76744626b5e8a17ad7db
Mauricio Fierro <mauriciofierrom@gmail.com>
2024-04-22 21:30:26 -0500

    Configure authgear

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import authgear from "@authgear/web";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+async function init() {
+  try {
+    await authgear.configure({
+      endpoint: "https://trello2notion.authgear.cloud",
+      clientID: "123f6884f558297e",
+      sessionType: "refresh_token"
+    });
+  } finally {
+    ReactDOM.createRoot(
+      document.getElementById('root') as HTMLElement
+    ).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  }
+}
+
+init().catch((e) => {
+  console.error(e)
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
